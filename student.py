0d64af5cc34b69058f7682c676d6e42178fa197c
calebD21 <100421624+calebD21@users.noreply.github.com>
2022-03-29 08:38:27 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,6 +1,17 @@
 def main():
     #your code goes here
-    print("0")
+    from re import X
+
+
+    x = input()
+    y = int(x)
+
+    if y%3 == 0 and y%5 == 0:
+    print("fizzbuzz")
+    elif y%5 == 0:
+        print("buzz")
+    elif y%3 == 0:
+        print("fizz")
 
 if __name__ =='__main__':
     main()
